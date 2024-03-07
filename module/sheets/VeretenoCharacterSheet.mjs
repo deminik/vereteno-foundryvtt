6b8b98ca42dc77e0289db1525aea78b0f70bd666
deminik <n.i.demidov@gmail.com>
2024-03-07 16:13:07 +0400

    добавил броски из чарника

--- module/sheets/VeretenoCharacterSheet.mjs
+++ module/sheets/VeretenoCharacterSheet.mjs
@@ -11,6 +11,14 @@ export class VeretenoCharacterSheet extends ActorSheet {
         return `systems/vereteno/templates/sheets/${this.actor.type}-sheet.hbs`;
     }
 
+    activateListeners(html) {
+        super.activateListeners(html);
+
+        if (!this.isEditable) return;
+
+        html.on('click', '.rollable', this._onRoll.bind(this));
+    }
+
     getData() {
         const context = super.getData();
 
@@ -66,5 +74,87 @@ export class VeretenoCharacterSheet extends ActorSheet {
         system.stats.reputation.label = game.i18n.localize(CONFIG.vereteno.stats.reputation);
 
         return system;
+    }
+
+    async _onRoll(event) {
+        event.preventDefault();
+        const element = event.currentTarget;
+        const dataset = element.dataset;
+
+        let { label, rollKey, rollType } = dataset;
+
+        let messageData = {
+            user: game.user._id,
+            speaker: ChatMessage.getSpeaker(),
+            flavor: label
+        };
+
+        let actorRollData = await this._prepareActorRollData(rollType, rollKey);
+
+        let roll = new Roll(actorRollData.pool + actorRollData.dice);
+
+        roll.toMessage(messageData);
+    }
+
+    async _prepareActorRollData(type, key) {
+        switch (type) {
+            case "attribute":
+                return await this._prepareAttributeRollData(key);
+            case "skill":
+                return await this._prepareSkillRollData(key);
+                break;
+        }
+    }
+
+    async _getContext() {
+        return this.object.system;
+    }
+
+    async _prepareAttributeRollData(key) {
+        let context = await this._getContext();
+
+        let attribute = context.attributes[key];
+
+        if (!attribute) {
+            return null;
+        }
+
+        let attributeValue = attribute.value;
+
+        let attributeBonuses = 0;
+
+        let attributePoolResult = attributeValue + attributeBonuses;
+
+        let rollData = {
+            "dice": "d20",
+            "pool": attributePoolResult
+        };
+
+        return rollData;
+    }
+
+    async _prepareSkillRollData(key) {
+        let context = await this._getContext();
+
+        let skill = context.skills[key];
+
+        if (!skill) {
+            return null;
+        }
+
+        let skillAttributeKey = skill.attribute;
+        let skillAttributeRollData = await this._prepareAttributeRollData(skillAttributeKey);
+
+        let skillValue = skill.value;
+
+        let skillBonuses = 0;
+
+        let skillPoolResult = skillValue + skillBonuses;
+
+        let rollData = skillAttributeRollData;
+        rollData.dice = "d20";
+        rollData.pool = rollData.pool + skillPoolResult;
+
+        return rollData;
     }
 }
