5dd85f4ef60b2a992a2461d9b7947b8885e432b5
deminik <n.i.demidov@gmail.com>
2024-03-14 19:50:23 +0400

    конфиг

---
+++ src/veretenoConfig.ts
@@ -0,0 +1,50 @@
+export const VeretenoConfig = {
+    tabs: {
+        stat: "vereteno.tab.stat",
+        equipment: "vereteno.tab.equipment",
+        fight: "vereteno.tab.fight",
+        bio: "vereteno.tab.bio"
+    },
+    attackTypes: {
+        none: "vereteno.attack.none",
+        brawling: "vereteno.attack.brawling",
+        melee: "vereteno.attack.melee",
+        ranged: "vereteno.attack.ranged",
+    },
+    rangeTypes: {
+        pointBlank: "vereteno.range.pointBlank",
+        close: "vereteno.range.close",
+        medium: "vereteno.range.medium",
+        long: "vereteno.range.long",
+        utmost: "vereteno.range.utmost"
+    },
+    stats: {
+        hitPoints: "vereteno.stat.hitPoint",
+        willPoints: "vereteno.stat.willPoint",
+        reputation: "vereteno.stat.reputation"
+    },
+    attributes: {
+        constitution: "vereteno.attribute.constitution",
+        intelligence: "vereteno.attribute.intelligence",
+        dexterity: "vereteno.attribute.dexterity",
+        empathy: "vereteno.attribute.empathy"
+    },
+    skills: {
+        melee: "vereteno.skill.melee",
+        strength: "vereteno.skill.strength",
+        agility: "vereteno.skill.agility",
+        piloting: "vereteno.skill.piloting",
+        stealth: "vereteno.skill.stealth",
+        ranged: "vereteno.skill.ranged",
+        cybershamanism: "vereteno.skill.cybershamanism",
+        survival: "vereteno.skill.survival",
+        medicine: "vereteno.skill.medicine",
+        observation: "vereteno.skill.observation",
+        science: "vereteno.skill.science",
+        mechanics: "vereteno.skill.mechanics",
+        manipulation: "vereteno.skill.manipulation",
+        leadership: "vereteno.skill.leadership",
+        witchcraft: "vereteno.skill.witchcraft",
+        culture: "vereteno.skill.culture"
+    }
+}
